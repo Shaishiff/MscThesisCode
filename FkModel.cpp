368882b149ccf4e4ca7a47a621e5ac948d952e9e
Shai <shai@crossrider.com>
2012-03-18 12:08:43 +0200

    - adding initial files

---
+++ FkModel.cpp
@@ -0,0 +1,422 @@
+// FkModel.cpp : Defines the entry point for the console application.
+//
+
+#include "defs.h"
+#include "FkModel.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+CFkModel::CFkModel()
+{
+	dVdh = 0.0;
+	dVdw = 0.0;
+	dW2 = dW*dW;
+	dH2 = dH*dH;
+	mat = NULL;
+	fibroMat = NULL;
+	new_mat = NULL;
+	s_mat = NULL;
+	new_s_mat = NULL;
+	f_mat = NULL;
+	new_f_mat = NULL;
+	rise_time_mat = NULL;
+
+	InitFkModel();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+CFkModel::~CFkModel()
+{
+	DeleteFkModel();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::InitFkModel()
+{
+	mat = new double*[Nh+2];
+	fibroMat = new double*[Nh+2];
+	new_mat = new double*[Nh+2];
+	s_mat = new double*[Nh+2];
+	new_s_mat = new double*[Nh+2];
+	f_mat = new double*[Nh+2];
+	new_f_mat = new double*[Nh+2];
+	rise_time_mat = new double*[Nh+2];
+	for(int iH = 0; iH < Nh+2; ++iH)
+	{
+		mat[iH] = new double[Nw+2];
+		fibroMat[iH] = new double[Nw+2];
+		new_mat[iH] = new double[Nw+2];
+		s_mat[iH] = new double[Nw+2];
+		new_s_mat[iH] = new double[Nw+2];
+		f_mat[iH] = new double[Nw+2];
+		new_f_mat[iH] = new double[Nw+2];
+		rise_time_mat[iH] = new double[Nw+2];
+		for(int iW = 0; iW < Nw+2; ++iW)
+		{
+			mat[iH][iW] = 0.0;			
+			new_mat[iH][iW] = 0.0;
+			s_mat[iH][iW] = 0.0;
+			new_s_mat[iH][iW] = 0.0;
+			f_mat[iH][iW] = 0.0;
+			new_f_mat[iH][iW] = 0.0;
+			fibroMat[iH][iW] = 0.0;
+			rise_time_mat[iH][iW] = 0.0;
+
+			if((iH == 0) || (iH == Nh+1) || (iW == 0) || (iW == Nw+1))
+			{
+				fibroMat[iH][iW] = 1;
+			}
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::InitTargetFibroblastMat()
+{
+	InitTargetFibroblastMat(5, 5, 25, 25);
+}
+
+void CFkModel::InitTargetFibroblastMat(int nCenterH, int nCenterW, int nHeight, int nWidth)
+{
+	AddFibroblasts(nCenterH,nCenterH+nHeight,nCenterW,nCenterW+nWidth);	
+	SaveToInputFile(fibroMat, "TargetFibroblastMat.txt");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::DeleteFkModel()
+{
+	for(int iH = 0; iH < Nh+2; ++iH)
+	{
+		delete [] mat[iH];
+		delete [] fibroMat[iH];
+		delete [] new_mat[iH];
+		delete [] s_mat[iH];
+		delete [] new_s_mat[iH];
+		delete [] f_mat[iH];
+		delete [] new_f_mat[iH];
+		delete [] rise_time_mat[iH];
+	}
+
+	delete [] mat;
+	delete [] fibroMat;
+	delete [] new_mat;
+	delete [] s_mat;
+	delete [] new_s_mat;
+	delete [] f_mat;
+	delete [] new_f_mat;
+	delete [] rise_time_mat;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::CleanupFkModel()
+{
+	for(int iH = 0; iH < Nh+2; ++iH)
+	{
+		for(int iW = 0; iW < Nw+2; ++iW)
+		{
+			mat[iH][iW] = 0;			
+			new_mat[iH][iW] = 0;
+			s_mat[iH][iW] = 0;
+			new_s_mat[iH][iW] = 0;
+			f_mat[iH][iW] = 0;
+			new_f_mat[iH][iW] = 0;
+			rise_time_mat[iH][iW] = 0;
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::AddFibroblasts(int hStart, int hEnd, int wStart, int wEnd)
+{
+	if((hStart <= 0) || (hEnd >= Nh+1) || (wStart <= 0) || (wEnd >= Nw+1))
+	{
+		return;
+	}
+
+	for (int iH=hStart; iH <= hEnd; ++iH)
+	{
+		for (int iW=wStart; iW <= wEnd; ++iW)
+		{
+			fibroMat[iH][iW] = 1;
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+bool CFkModel::SaveToInputFile(double** mat, char* fileName)
+{
+	char fullFilePath[FILE_NAME_BUFFER_SIZE] = {0};
+	sprintf(fullFilePath, "C:\\Users\\Shai\\Documents\\MATLAB\\MSc\\FkModel\\Input\\%s", fileName);
+	return SaveToFile(mat, fullFilePath);
+}
+
+bool CFkModel::SaveToOutputFile(double** mat, char* fileName)
+{
+	char fullFilePath[FILE_NAME_BUFFER_SIZE] = {0};
+	sprintf(fullFilePath, "C:\\Users\\Shai\\Documents\\MATLAB\\MSc\\FkModel\\Output\\%s", fileName);
+	return SaveToFile(mat, fullFilePath);
+}
+
+bool CFkModel::SaveToFile(double** mat, char* fileName)
+{
+	FILE* pFile = fopen(fileName, "w");
+	if(pFile == NULL)
+	{
+		return false;
+	}
+
+	for (int iH = 1; iH < Nh+1; ++iH)
+	{
+		for (int iW = 1; iW < Nw+1; ++iW)
+		{
+			fprintf(pFile, "%f ", mat[iH][iW]);
+		}
+		fprintf(pFile, "\n");
+	}
+	fclose(pFile);
+	return true;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const double SaveToFileInterval = 5.0;
+
+void CFkModel::SaveToFile(int iT, int& nFileNumber, double** mat, char* nameOfVar)
+{
+#ifndef SAVE_FK_MODEL_TO_FILE
+	return;
+#endif
+
+	if((nFileNumber*SaveToFileInterval) <= (iT*dt))
+	{		
+		char fileName[FILE_NAME_BUFFER_SIZE] = {0};
+		int n = sprintf(fileName, "C:\\Users\\Shai\\Documents\\MATLAB\\MSc\\FkModel\\Output\\SimulationOutput_%s_%d.txt", nameOfVar, nFileNumber);
+		if(SaveToFile(mat, fileName))
+		{
+			++nFileNumber;
+			// printf("Saved data to file %d at time: %.4f\n", nFileNumber-1, (iT*dt));
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void CFkModel::CalculateDer(int iH, int iW, double** inFibroblastMat)
+{
+	if(inFibroblastMat[iH-1][iW] == 1)
+		dVdh = (mat[iH+1][iW] - mat[iH][iW])/dH2;
+	else if (inFibroblastMat[iH + 1][iW] == 1)
+		dVdh = (mat[iH-1][iW] - mat[iH][iW])/dH2;
+	else
+		dVdh = (mat[iH-1][iW] + mat[iH+1][iW] - 2*mat[iH][iW])/dH2;
+
+	if(inFibroblastMat[iH][iW-1] == 1)
+		dVdw = (mat[iH][iW+1] - mat[iH][iW])/dW2;
+	else if (inFibroblastMat[iH][iW+1] == 1)
+		dVdw  = (mat[iH][iW-1] - mat[iH][iW])/dW2;
+	else
+		dVdw  = (mat[iH][iW-1] + mat[iH][iW+1] - 2*mat[iH][iW])/dW2;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+double** CFkModel::ExecuteFkModel(double** inFibroblastMat, const ProtocolParams& protParams)
+{	
+	CleanupFkModel();
+	double t_ung = 0.0;
+	double J_ung = 0.0;
+	double s_inf = 0.0;
+	double tau_s = 0.0;
+	double J_slow = 0.0;
+	double f_inf = 0.0;
+	double tau_f = 0.0;
+	double J_fast  = 0.0;
+	double Jion = 0.0;
+	int nFibroFileNumber = 0;
+	int nuFileNumber = 0;
+	int nsFileNumber = 0;
+	int nfFileNumber = 0;
+
+	if(inFibroblastMat == NULL)
+	{
+		inFibroblastMat = fibroMat;
+	}
+
+	SaveToFile(0, nFibroFileNumber, inFibroblastMat, "Fibroblasts"); 
+
+	// Start the temporal loop.
+	for (int iT = 0; iT < Nt; ++iT)
+	{   		
+		// Save the current data to the file.	
+		//SaveToFile(iT, nuFileNumber, mat, "u"); 
+		//SaveToFile(iT, nsFileNumber, s_mat, "s"); 
+		//SaveToFile(iT, nfFileNumber, f_mat, "f"); 
+		
+		// Calc the time in ms and check if we need to use the S1 protocol.
+		double curTime = iT*dt;
+		bool bS1 = ((curTime >= protParams.m_BeginTime) && (curTime <= (protParams.m_BeginTime+protParams.m_TotalTime)));
+		
+		// Start the spatial loop.
+		for (int iH = 1; iH < Nh+1; ++iH)
+		{
+			for (int iW = 1; iW < Nw+1; ++iW)
+			{            
+				if(inFibroblastMat[iH][iW] == 1)
+				{
+					rise_time_mat[iH][iW] = TotalSimulationTime*2;
+				}
+				else
+				{
+					CalculateDer(iH, iW, inFibroblastMat);
+					
+					double Jstim = 0.0;
+					if(bS1)
+					{
+						double curH = iH*dH;
+						double curW = iW*dW;
+						if(curH >= protParams.m_hStart && curH <= protParams.m_hEnd && curW >= protParams.m_wStart && curW <= protParams.m_wEnd)
+						{
+							Jstim = S1Amp;
+						}
+					}
+
+					double v = mat[iH][iW];
+					if((v > 0.95) && (rise_time_mat[iH][iW] == 0.0))
+					{
+						if(Jstim == 0.0)
+						{
+							rise_time_mat[iH][iW] = iT*dt;
+						}
+					}
+
+					double s = s_mat[iH][iW];
+					double f = f_mat[iH][iW];
+
+					 // Ungated  Outward  Current
+					if (v < V_u)
+					{
+						t_ung = t_ug1; // tau_ungated
+						J_ung = (1 / t_ung) * v; // J_ungated
+					}
+					else // v > V_u
+					{
+						t_ung =t_ug2; // tau_ungated
+						J_ung = (1 / t_ung) * 1; // J_ungated
+					}
+
+					// Slow  Gate  Current
+					if (v < V_s)
+					{
+						s_inf = 1;
+						tau_s = ts_opn;
+					}
+					else // v > V_s
+					{
+						s_inf = 0;
+						tau_s = ts_cls;
+					}
+
+					// Slow  Inward  Current
+					J_slow = -(s / t_slow) * (1+tanh(kwm * (v - V_slow) ) ) / 2;
+
+					// Fast  Gate  Current
+					if (v < V_f)
+					{
+						f_inf = 1;
+						if (v < Vf_open)
+							tau_f = tf_opn1;
+						else // v > Vf_open
+							tau_f = tf_opn2;
+					}
+					else // v < V_f
+					{
+						f_inf = 0;
+						tau_f = tf_cls;
+					}
+
+					// Fast  Inward  Current
+					if (v < V_f)
+					{
+						J_fast = 0;
+					}
+					else
+					{
+						J_fast = -(f / t_fast) * (v - V_f) * (1 - v);
+					}
+
+					Jion = J_fast + J_slow + J_ung;
+
+					new_mat[iH][iW] = mat[iH][iW] + ((dt*sigma)/(Cm*Am))*(dVdh + dVdw) - (dt/Cm)*Jion + (dt/(Am*Cm))*Jstim;
+					new_s_mat[iH][iW] = s_mat[iH][iW] + dt*(s_inf - s)/tau_s;                        
+					new_f_mat[iH][iW] = f_mat[iH][iW] + dt*(f_inf - f)/tau_f;                
+				}
+			}
+		}
+		
+		// Move the current mats to be the old ones.
+		double** temp = NULL;
+
+		temp = mat;
+		mat = new_mat;
+		new_mat = temp;
+
+		temp = s_mat;
+		s_mat = new_s_mat;
+		new_s_mat = temp;
+
+		temp = f_mat;
+		f_mat = new_f_mat;
+		new_f_mat = temp;
+	}
+
+	//SaveToFile(Nt, nuFileNumber, mat, "u"); 
+	//SaveToFile(Nt, nsFileNumber, s_mat, "s"); 
+	//SaveToFile(Nt, nfFileNumber, f_mat, "f");
+	int n = 0;
+	SaveToFile(Nt, n, rise_time_mat, "r");
+
+	// return mat;
+	return rise_time_mat;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand(time(NULL));
+	clock_t mainStartingTime = clock();
+	CFkModel* pModel = new CFkModel();
+	pModel->InitTargetFibroblastMat();
+	pModel->ExecuteFkModel(NULL);
+	clock_t mainEndingTime = clock();
+	double mainRunningTime = (mainEndingTime - mainStartingTime)/double(CLOCKS_PER_SEC);
+	printf("Main duration: %.3f seconds", mainRunningTime);
+	delete pModel;
+	pModel = NULL;
+	_CrtDumpMemoryLeaks();
+	//getchar();
+	return 0;
+}
+*/
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
---
+++ defs.h
@@ -0,0 +1,146 @@
+
+#ifndef __DEFS_H_
+#define __DEFS_H_
+
+#define USE_MPI
+#define FILE_NAME_BUFFER_SIZE	1024
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Includes
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "math.h"
+#include "time.h"
+#ifndef USE_MPI
+#include "targetver.h"
+#include <tchar.h>
+#include "windows.h"
+#endif
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// GA defines and consts
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const int TargetCenterH = 5;
+const int TargetCenterW = 5;
+const int TargetHeight = 25;
+const int TargetWidth = 25;
+
+#define Npop 10
+#define MaxIterations 1000
+#define SelectionRate 0.4
+#define NsurvivingPopulation (int)floor(Npop*SelectionRate +0.5)
+#define Nmates (Npop - NsurvivingPopulation)
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// // Physiological consts
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const double Cm = 1.0; // membrance capacitance - microF/cm^2
+const double Am = 3000.0; // 1/cm
+const double sigma = 0.75; // 0.3; 3.0*10^-4; mS/cm
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// APD & CV Restitution
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const double t_fast  = 0.249; // tau_fast
+const double tf_opn1 =  40; // tau_f_open_1
+const double tf_opn2 =  82.5; // tau_f_open_2
+const double tf_cls =  5.75; // tau_f_close
+const double t_slow =226.9; // tau_slow
+const double ts_opn =  100; // tau_s_open
+const double ts_cls =  300; // tau_s_close
+const double t_ug1 = 64.7; // tau_ung_1
+const double t_ug2 = 222.9; // tau_ung_2
+const double Vrest = -81.2; // in  mV
+const double Vpeak = 3.6; // in  mV
+const double V_f = 0.16;  
+const double V_s = V_f; 
+const double V_u = V_f;
+const double Vf_open = 0.04;
+const double V_slow  =  0.85;
+const double kwm = 10;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Simulation consts
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Time parameters:
+const double dt = 0.02; // millisec
+const double TotalSimulationTime = 200; // Total time of simulation
+const int Nt = (int)(ceil(TotalSimulationTime/dt));
+
+// Space parameters
+#define dW 0.05 // cm/node
+#define dH 0.05 // cm/node
+#define W 2 // Width, x[cm]
+#define H 2 // Height, y[cm]
+const int Nw = (int)ceil(W/dW);
+const int Nh = (int)ceil(H/dH);
+
+struct ProtocolParams
+{
+	double m_Amp;
+	double m_TotalTime;
+	double m_BeginTime;
+	double m_hStart;
+	double m_hEnd;
+	double m_wStart;
+	double m_wEnd;
+};
+
+// Protocol parameters:
+const double S1Amp = 500.0; // microA/cm^3
+const double S1TotalTime = 5; // milliseconds
+const double S1BeginTime = 5.0; // milliseconds
+const double S1hStart = 0.0;
+const double S1hEnd = dH;
+const double S1wStart = 0.0;
+const double S1wEnd = W;
+
+struct S1Protocol : public ProtocolParams
+{
+	S1Protocol()
+	{
+		m_Amp = S1Amp;
+		m_TotalTime = S1TotalTime;
+		m_BeginTime = S1BeginTime;
+		m_hStart = S1hStart;
+		m_hEnd = S1hEnd;
+		m_wStart = S1wStart;
+		m_wEnd = S1wEnd;
+	}
+};
+
+const double S2Amp = 500.0; // microA/cm^3
+const double S2TotalTime = 5; // milliseconds
+const double S2BeginTime = 5.0; // milliseconds
+const double S2hStart = 0.0;
+const double S2hEnd = H;
+const double S2wStart = 0.0;
+const double S2wEnd = dW;
+
+struct S2Protocol : public ProtocolParams
+{
+	S2Protocol()
+	{
+		m_Amp = S2Amp;
+		m_TotalTime = S2TotalTime;
+		m_BeginTime = S2BeginTime;
+		m_hStart = S2hStart;
+		m_hEnd = S2hEnd;
+		m_wStart = S2wStart;
+		m_wEnd = S2wEnd;
+	}
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // __DEFS_H_
